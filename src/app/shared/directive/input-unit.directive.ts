75e43d601240b5a9c67bd534b91a24e58cef6389
quyendangbys <quyen.dang@bys.com.vn>
2019-11-20 14:30:43 +0700

    - thêm value vào control bằng directive

--- src/app/shared/directive/input-unit.directive.ts
+++ src/app/shared/directive/input-unit.directive.ts
@@ -1,20 +1,30 @@
 import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
-@Directive({
-  selector: '[appInputUnit]'
-})
+@Directive( {
+  selector: '[appInputUnit]',
+  // tslint:disable-next-line:no-host-metadata-property
+  host: {
+    '(blur)': 'formatInputValue($event.target.value)',
+}
+} )
 export class InputUnitDirective {
-  constructor(
+  constructor (
     private el: ElementRef,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private ngControl: NgControl,
   ) {
   }
-  @HostListener('mouseenter') onMouseEnter() {
+  @HostListener( 'mouseenter' ) onMouseEnter() {
     // this.el.nativeElement.innerText += 'Diem';
-    this.renderer.setProperty(this.el.nativeElement, 'innerHTML', '<h1>Hello world</h1>');
+    this.renderer.setProperty( this.el.nativeElement, 'innerHTML', '<h1>Hello world</h1>' );
   }
-  @HostListener('mouseleave') onMouseLeave() {
+  @HostListener( 'mouseleave' ) onMouseLeave() {
 
+  }
+
+  formatInputValue( value ) {
+    this.ngControl.valueAccessor.writeValue( value + 'Quang đẹp trai' );
   }
 
 
