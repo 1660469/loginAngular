5892eb2eb65e090c1c4f4c74f62d9514e29dc20c
1660469 <vovanquang110@gmail.com>
2019-11-27 10:03:49 +0700

    a

--- src/app/shared/directive/input-unit.directive.ts
+++ src/app/shared/directive/input-unit.directive.ts
@@ -11,20 +11,12 @@ import { NgControl } from '@angular/forms';
 export class InputUnitDirective {
   constructor (
     private el: ElementRef,
-    private renderer: Renderer2,
     private ngControl: NgControl,
   ) {
   }
-  @HostListener( 'mouseenter' ) onMouseEnter() {
-    // this.el.nativeElement.innerText += 'Diem';
-    this.renderer.setProperty( this.el.nativeElement, 'innerHTML', '<h1>Hello world</h1>' );
-  }
-  @HostListener( 'mouseleave' ) onMouseLeave() {
-
-  }
 
   formatInputValue( value ) {
-    this.ngControl.valueAccessor.writeValue( value + 'Quang đẹp trai' );
+    this.ngControl.valueAccessor.writeValue( value + ' Điểm' );
   }
 
 
