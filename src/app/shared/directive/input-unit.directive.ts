7a3b7bc6d33b0d83a9fb98d06a04551291a50a2f
1660469 <vovanquang110@gmail.com>
2019-11-27 11:26:32 +0700

    done

--- src/app/shared/directive/input-unit.directive.ts
+++ src/app/shared/directive/input-unit.directive.ts
@@ -1,23 +1,42 @@
-import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2, Input } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
-@Directive( {
+@Directive({
   selector: '[appInputUnit]',
   // tslint:disable-next-line:no-host-metadata-property
   host: {
-    '(blur)': 'formatInputValue($event.target.value)',
-}
-} )
+    '(blur)': 'formatInputValue($event.target.value )',
+    '(focus)': 'formatInputValueFocus($event.target.value)',
+    '(keypress)': 'numberOnly($event)',
+  }
+})
 export class InputUnitDirective {
-  constructor (
+  valueFocus;
+  @Input() maxleng: number;
+  constructor(
     private el: ElementRef,
     private ngControl: NgControl,
   ) {
   }
 
-  formatInputValue( value ) {
-    this.ngControl.valueAccessor.writeValue( value + ' Điểm' );
+  formatInputValue(value) {
+    this.ngControl.valueAccessor.writeValue(value + ' Điểm');
   }
+  formatInputValueFocus(value) {
+    const valueNew = value.replace(/ Điểm/g, '');
+    this.ngControl.valueAccessor.writeValue(valueNew);
 
+  }
+  numberOnly(event): boolean {
+    if (this.el.nativeElement.value.length >= this.maxleng) {
+      event.preventDefault();
+    } else {
+      const charCode = (event.which) ? event.which : event.keyCode;
+      if (charCode > 31  && (charCode < 46 || charCode > 57 || charCode === 47)) {
+        return false;
+      }
+      return true;
+    }
+  }
 
 }
